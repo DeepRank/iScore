f0ea3808f1946e132195e10d8148c651c178f712
Nicolas Renaud <n.renaud@tudelft.nl>
2018-06-21 16:32:43 +0200

    new example

--- example/kernel/create_kernel.py
+++ example/kernel/create_kernel.py
@@ -3,17 +3,18 @@ from iScore.graph import Graph, iscore_graph
 from iScore.kernel import Kernel
 
 # create all the graphs of the pdb in ./pdb/
-#iscore_graph()
+iscore_graph()
+
+# comapre the graphs with the ones obtained
+# with the matlab version
+g = Graph('./graph/2OZA.pckl')
+gcheck = Graph('./check/graphMAT/2OZA.mat')
+g.compare(gcheck)
 
 
-# g = Graph('./graph/2OZA.pckl')
-# gcheck = Graph('./check/graphMAT/2OZA.mat')
-# g.compare(gcheck)
-
-
-# g = Graph('./graph/1IRA.pckl')
-# gcheck = Graph('./check/graphMAT/1IRA.mat')
-# g.compare(gcheck)
+g = Graph('./graph/1IRA.pckl')
+gcheck = Graph('./check/graphMAT/1IRA.mat')
+g.compare(gcheck)
 
 
 #init and load the data
@@ -21,7 +22,7 @@ ker = Kernel()
 ker.import_from_mat()
 
 # get the path of the check file
-#checkfile = ker.get_check_file(fname=None)
+checkfile = ker.get_check_file(fname='./check/kernelMAT/K.mat')
 
 # run the calculations
-ker.run(lamb=1.0,walk=4,check=None)
+ker.run(lamb=1.0,walk=4,check=checkfile)
