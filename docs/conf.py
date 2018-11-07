6765cf7a0ae08ced81ceb52adf652ce612ca8392
Nicolas Renaud <n.renaud@tudelft.nl>
2018-11-07 12:38:24 +0100

    mock plt

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['numpy', 'scipy','h5py','scipy.sparse','scipy.io','torch','tqdm']
+MOCK_MODULES = ['numpy', 'scipy','h5py','scipy.sparse','scipy.io','matplotlib.pyplot','tqdm']
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 sys.path.insert(0, os.path.abspath('../'))
