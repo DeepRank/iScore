c17d438dec94b7f3a94a0d300f950601cfc81b45
Nicolas Renaud <n.renaud@tudelft.nl>
2019-03-26 07:29:17 +0100

    global naming for pssm

--- iScore/graphrank/graph.py
+++ iScore/graphrank/graph.py
@@ -648,7 +648,12 @@ def iscore_graph(pdb_path='./pdb/',pssm_path='./pssm/',select=None,
         mol_name = os.path.splitext(name)[0]
 
         # get the pssm files
+
+
         mol_pssm = list(filter(lambda x : x.startswith(mol_name) and x.endswith('.pdb.pssm'),all_pssm_files))
+        if len(mol_pssm) == 0:
+            print('--> Assuming global naming scheme for pssm files')
+            mol_pssm = list(filter(lambda x : x.startswith(mol_name.split('_')[0]) and x.endswith('.pdb.pssm'),all_pssm_files))
         mol_pssm.sort()
         chain_label = [m.split('.')[-3] for m in mol_pssm]
 
