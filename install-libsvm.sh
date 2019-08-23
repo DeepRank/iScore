ff89a91d5e81163ed66a4a64e6e31ce306c03447
NicoRenaud <nicolas.gm.renaud@gmail.com>
2019-08-23 14:20:41 +0200

    removed install script

--- install-libsvm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ex
-wget https://github.com/cjlin1/libsvm/archive/v323.tar.gz
-tar -xvf v323.tar.gz
-cd libsvm-323/python
-make
-
-
-
--- install-pdb2sql.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ex
-git clone -b multiple_interface https://github.com/DeepRank/pdb2sql
-cd pdb2sql
-pip install -e ./
-
-
-
-
